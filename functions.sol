b940b23ff3b56a4fc37594080496d27e2d58e548
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-12-23 15:32:39 +0545

    functions

---
+++ functions.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity >=0.7.0;
+
+contract functions {         // this is not a function just the name of the contract
+
+    uint public val = 4;
+
+
+    function add() public pure returns(uint){
+        return 2+3;
+    }
+
+    
+} 
+
+
+// Pure ->> state variables edit
+// view ->> state variables view
+// public ->>
+// return ->> k return garxa function le tyo specify garnu parxa
