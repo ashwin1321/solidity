4a86d4fc72f9312dcb52174b73ef235c97669afe
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-12-23 14:45:19 +0545

    scope variables and access modifiers

---
+++ variables.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity >=0.7.0;
+
+contract variables {
+
+    // state variables  ==  variable whose values are permanently stored in a contract storage
+    // local variables == variables whose value is present till function is executing 
+    // global variables == special variable in global namespace used to get information about the blockchain
+
+
+    // NOTE: Solidity is statically typed language, so state or local variables needs to be specified during declaration. 
+    // NOTE: THERE IS NO CONCEPT OF UNDEFINED OR NULL
+
+
+
+    // SCOPE VARIABLES
+
+    // public  -- can be accessed internally as well as via message
+    // Internal -- can be accessed only internally from the current contract or contract deriving from it without using this.
+    // Private -- can be accessed only internally from current contract they are defined not in the derived contract from it.
+    
+} 
