c9f3c6419c9b114d2bea4c54c64f75189358d782
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-12-23 14:14:41 +0545

    value types

--- valueTypes.sol
+++ valueTypes.sol
@@ -20,6 +20,7 @@ contract Variable{
     bool active = true;
     bytes6 symbol = "ashwin";               // for x number of bytes define "bytesx"
     string name = "Variable Types in Solidity";
+    address add = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
 }
 
