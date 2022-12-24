f66f733beda2da5e6a7e4c7bf1f09fbd50fe169f
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-12-24 20:48:14 +0545

    public private internal variables

--- scopeVasriable.sol
+++ scopeVasriable.sol
@@ -10,6 +10,19 @@ pragma solidity >=0.7.0;
 
 contract scopeVariables {      
     
+    uint public val = 4;                // public ho tei bhara deploy garda directly call garna milxa
+    function get() public view returns(uint){
+        return val3;           // internally in the program call hunxa yesari
+    }
     
-    
+
+    uint internal val2 = 5;             // internally and derived calss lai matra access hunxa
+    uint private val3 = 1;              // yo aba yei contract bhitra matra access hunxa
 } 
+
+contract scopeVariables2 is scopeVariables {              // inherit gareko ho la. multiple inherit garna , le seperate garne
+
+     function get2() public view returns(uint){
+        return val2;           // derived class le access garna payo internal variable lai
+    }
+}
