4d517f25a336f8e61b97d3ebdb2b61000e5158e0
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-12-27 18:33:15 +0545

    inheritance

---
+++ inheritance.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: Unlicensed
+
+import "hashing.sol";          // yeasri aru file import ni garna milxa locally
+
+// import "http/asgasgagagagagaa"   // external file ni import garna milxa github bata
+
+pragma solidity >=0.7.0;
+
+contract Base{
+    
+    uint internal num;
+
+    constructor (uint _num){
+        num = _num;
+    }
+}
+
+// contract Child1 is Base(3){                 // constructor ko value yesari nai dina ni milxa OR,
+contract Child1 is Base{
+
+    constructor () Base(5){                    // OR, yesari ni dina milxa
+
+    }
+
+    function getVal() public view returns(uint){
+        return num;
+    }
+}
+
+
+contract child2 is hashing{            // hash bhanne file import gareko xa, ani tei file bhitra ko contract ko naam hashing ho la
+
+
+}
