f0593fe4b8036cb0fa2460a66cfc054b252272e9
ashwin1321 <ashwinkhatiwada7@gmail.com>
2022-12-27 19:21:10 +0545

    interface

---
+++ interfaces.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Unlicensed
+
+
+// similar to abstract contracts BUT no function with any implementation can be defined
+// function are type external
+// no state variables or constructors can be defined
+// Interface can have enums and structs
+// by default virtual hunxa override garna abstract contract jasto virtual keyword dirakhnnu pardaina
+
+pragma solidity >=0.7.0;
+
+interface base{
+
+    function get() external pure returns(uint);
+}
+
+contract main is base{          // inherit garna milxa
+
+    function get() public pure override returns(uint){
+        return 2;
+    }
+}
